4a65972d0b1c93929bc45060cd52acdb8f4119a4
yl354@duke.edu <yl354@duke.edu>
2016-04-01 14:42:24 -0400

    Initial the NumberWizard Project

---
+++ NumberWizard/Assets/NumberWizard.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NumberWizard : MonoBehaviour {
+
+	int max = 1000;
+	int min = 1;
+	int guess = 500;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
