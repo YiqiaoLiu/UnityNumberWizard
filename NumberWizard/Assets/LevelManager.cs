55b15b7418a6f7a6dcf1915eba1a05cc633dc858
yl354@duke.edu <yl354@duke.edu>
2016-04-05 10:02:56 -0400

    Improve the level manager

--- NumberWizard/Assets/LevelManager.cs
+++ NumberWizard/Assets/LevelManager.cs
@@ -5,9 +5,11 @@ public class LevelManager : MonoBehaviour {
 
 	public void LoadLevel(string name) {
 		Debug.Log ("A level load requested to: " + name);
+		Application.LoadLevel(name);
 	}
 
 	public void QuitRequest() {
 		Debug.Log("I want to quit the game!");
+		Application.Quit();
 	}
 }
